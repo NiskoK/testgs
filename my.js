9b1b27648d4f8a584bfc2dd077c37fa43c9602af
NiskoK <100468074+NiskoK@users.noreply.github.com>
2022-03-16 12:33:07 +0100

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,5 @@
 
-function testGS(){
+function testGS(){//read from the spreadsheet!!!
     
     const url = "https://script.google.com/macros/s/AKfycbxOP35dXJ0v7gPMDIZ7e_1nHVwNSS6c4hiPmRdIyx8GcbXcYpaDjgio6nMcsqxvxy7xBA/exec";
     
@@ -14,3 +14,51 @@ function testGS(){
 }
 
 document.getElementById("btn").addEventListener("click",testGS);
+
+
+document.getElementById("btn2").addEventListener("click",addGS);
+
+//to write something
+function addGS(){
+//we will read the same url but we will do a post request!
+    const url = "https://script.google.com/macros/s/AKfycbxOP35dXJ0v7gPMDIZ7e_1nHVwNSS6c4hiPmRdIyx8GcbXcYpaDjgio6nMcsqxvxy7xBA/exec";
+    
+    fetch(url,{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({name:"Olivia"}) // body data type must match "Content-Type" header
+      });
+}
+
+
+/*
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  postData('https://example.com/answer', { answer: 42 })
+    .then(data => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+*/
